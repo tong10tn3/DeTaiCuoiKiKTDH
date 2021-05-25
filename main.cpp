a8919966c5c15ef393ebc98f036fb122b12c63d2
Tong <tong10tn3@gmail.com>
2021-05-25 16:16:25 +0700

    3d3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <graphics.h> 
 #include "VeGiaoDien.h"
+#include "ve3D3.h"
 using namespace std;
 
 int main()//int argc, char *argv[]
 {
 	initwindow(1280+8,720+12);
 	setGiaoDien();
-	xuLi();
+	//xuLi();
+	hamVeHinhHopChuNhat();
 	getch();
 	return 0;
 	
--- ve3D3.h
+++ ve3D3.h
@@ -1,12 +1,1 @@
-
-void xuLi3D1();
-void VeHinhChuNhatVien(int x, int y, int z, int b, int color);
-void LineDDA();
-void HeTrucXYZ();
-void textWrite(int &x, int &y, char s[], int color);
-void textWriteRemove(int &x, int &y, char s[]);
-void textWriteln(int &x, int &y, char s[]);
-void textReadnumber(int &x, int &y, char s[]);
-void LineDDAan(int x1, int y1, int x2, int y2);
-//void hientoado(int sd, toado2D A[],int couser);
-void HinhHopCN();
+void hamVeHinhHopChuNhat();
